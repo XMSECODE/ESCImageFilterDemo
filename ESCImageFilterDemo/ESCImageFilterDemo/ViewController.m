ea1b16afa4b3715a2862018b6159d137c8103056
xiang <850794895@qq.com>
2019-05-06 10:20:20 +0800

    实现放大缩小

--- ESCImageFilterDemo/ESCImageFilterDemo/ViewController.m
+++ ESCImageFilterDemo/ESCImageFilterDemo/ViewController.m
@@ -14,6 +14,8 @@
 
 @property (weak, nonatomic) IBOutlet UIImageView *filterImageView;
 
+@property(nonatomic,strong)UIImage* oldImage;
+
 @end
 
 @implementation ViewController
@@ -24,32 +26,25 @@
     self.oldImageView.layer.borderWidth = 1;
     self.filterImageView.layer.borderWidth = 1;
     
+    self.oldImage = [UIImage imageNamed:@"test2"];
+    self.oldImageView.image = self.oldImage;
     
+    [self filterTest];
     
-    // Do any additional setup after loading the view.
+    [self testRotate];
+  
+}
+
+- (void)filterTest {
+    double startTime = CFAbsoluteTimeGetCurrent();
+
+    CGImageRef imageRef = self.oldImage.CGImage;
     
-    UIImage *testImage = [UIImage imageNamed:@"test"];
-    
-    self.oldImageView.image = testImage;
-    
-    CGImageRef imageRef = testImage.CGImage;
     CIImage *ciimage = [CIImage imageWithCGImage:imageRef];
+    CIFilter *filter = [CIFilter filterWithName:@"CIBoxBlur"];
     
-    NSArray *array = [CIFilter filterNamesInCategories:nil];
-    
-//    NSLog(@"%@",array);
-    
-    CIFilter *filter = [CIFilter filterWithName:@"CIAffineTransform"];
     // 这里我们使用的是KVC的方式给filter设置属性
     [filter setValue:ciimage forKey:kCIInputImageKey];
-    
-    CGAffineTransform d3d = CGAffineTransformIdentity;
-    d3d = CGAffineTransformRotate(d3d, M_PI / 4 );
-//    d3d = CGAffineTransformScale(d3d, 2, 1);
-    
-    NSValue *value = [NSValue valueWithBytes:&d3d objCType:@encode(CGAffineTransform)];
-    [filter setValue:value forKey:kCIInputTransformKey];
-    
     //    3.有一个CIContext的对象去合并原图和滤镜效果
     CIImage *outputImage = filter.outputImage;
     //      创建一个图像操作的上下文
@@ -60,10 +55,48 @@
      *  fromRect:  合成之后图像的尺寸： 图像.extent
      */
     CGImageRef imageRefr = [context createCGImage:outputImage fromRect:outputImage.extent];
+    double endTime = CFAbsoluteTimeGetCurrent();
+    double time = endTime - startTime;
+    NSLog(@"%f",time);
     
-    self.filterImageView.image = [UIImage imageWithCGImage:imageRefr];
-    NSLog(@"%@===%@",self.oldImageView.image,self.filterImageView.image);
+    
+//    self.filterImageView.image = [UIImage imageWithCGImage:imageRefr];
 }
 
+- (void)testRotate {
+    double startTime = CFAbsoluteTimeGetCurrent();
+    
+        CGImageRef imageRef = self.oldImage.CGImage;
+    
+    CGAffineTransform d3d = CGAffineTransformIdentity;
+    d3d = CGAffineTransformRotate(d3d, M_PI / 4 );
+    d3d = CGAffineTransformScale(d3d, 0.3, 0.3);
+    CIImage *outputImage = [CIImage imageWithCGImage:imageRef];
+    
+    outputImage = [outputImage imageByApplyingTransform:d3d];
+    //      创建一个图像操作的上下文
+    CIContext *context = [CIContext contextWithOptions:nil];
+    //      把原图和滤镜合并起来  包含原图和滤镜效果的图像
+    /**
+     *  Image :   合成之后的图像
+     *  fromRect:  合成之后图像的尺寸： 图像.extent
+     */
+//    CGImageRef imageRefr = [context createCGImage:outputImage fromRect:outputImage.extent];
+    int centerX = outputImage.extent.origin.x + outputImage.extent.size.width / 2;
+    int centerY = outputImage.extent.origin.y + outputImage.extent.size.height / 2;
+    int startX = centerX - self.oldImage.size.width / 2;
+    int startY = centerY - self.oldImage.size.height / 2;
+    
+    CGImageRef imageRefr = [context createCGImage:outputImage fromRect:CGRectMake(startX, startY, self.oldImage.size.width, self.oldImage.size.height)];
+    
+    UIImage *newImage = [UIImage imageWithCGImage:imageRefr];
+    NSLog(@"%@===%@",outputImage,newImage);
+    self.filterImageView.image = newImage;
+    double endTime = CFAbsoluteTimeGetCurrent();
+    double time = endTime - startTime;
+    NSLog(@"%f",time);
+
+//    NSLog(@"%@===%@",self.oldImageView.image,self.filterImageView.image);
+}
 
 @end
